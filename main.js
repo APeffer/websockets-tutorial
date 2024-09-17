3efdfe3e24aaadf1b43dbe7641539d44a129ac7c
AyyPeff <log.alexpeffer@gmail.com>
2024-09-17 01:09:24 -0500

    transmit from server to browser

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 import { createBoard, playMove } from "./connect4.js";
 
+function showMessage(message) {
+    window.setTimeout(() => window.alert(message), 50);
+}
+
+function recieveMoves(board, websocket) {
+    websocket.addEventListener("message", ({ data }) => {
+        const event = JSON.parse(data);
+        switch (event.type) {
+            case "play":
+                //Update the UI with the move.
+                playMove(board, event.player, event.column, event.row);
+                break;
+            case "win":
+                showMessage(`Player ${event.player} wins!`)
+                // No further messages are expected; close the WebSocket connection.
+                websocket.close(1000);
+                break;
+            case "error":
+                showMessage(event.message);
+                break;
+            default:
+                throw new Error(`Unsupported event type: ${event.type}.`);
+        }
+    })
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     // Initialize the UI.
     const board = document.querySelector(".board");
     createBoard(board);
     // Open the WebSocket connection and register event handlers.
     const websocket = new WebSocket("ws://localhost:8001/");
+    recieveMoves(board, websocket);
     sendMoves(board, websocket);
   });
 
