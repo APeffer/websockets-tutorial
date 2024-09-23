8cefd8e40fb357134c67fc9b85eed58dd7f59e92
Alex Peffer <log.alexpeffer@gmail.com>
2024-09-22 23:13:48 -0500

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createBoard, playMove } from "./connect4.js";
 
 function getWebSocketServer() {
-  if (window.location.host === "apeffer.github.io/websockets-tutorial") {
+  if (window.location.host === "apeffer.github.io") {
     return "wss://websockets-tutorial-e64cd369e85d.herokuapp.com/";
   } else if (window.location.host === "localhost:8000") {
     return "ws://localhost:8001/";
